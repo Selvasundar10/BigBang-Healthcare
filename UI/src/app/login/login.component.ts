d61fee4b707ce88c2afd9c3c58dc6cd268ce5364
selvasundar10 <selvasundar.kanini@gmail.com>
2023-06-30 13:45:08 +0530

    Login Component

---
+++ UI/src/app/login/login.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { Router, RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent {
+
+
+  
+ 
+}
